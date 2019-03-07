982079db404ee7f296e0c6c18136057977397dcb
C. Jordan Ball III <cjordanball@ballwebdev.com>
2019-03-07 00:35:03 -0500

    add mongodb connection

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const schema = require('./schema/schema');
 const app = express();
 
+const MONGO_URI = 'mongodb://cjb:1password@ds161345.mlab.com:61345/scores'
+mongoose.Promise = global.Promise
+mongoose.connect(MONGO_URI);
+mongoose.connection
+	.once('open', () => console.log('We are now connected!'))
+	.on('error', error => console.log('ERROR CONNECTING TO MONGO: ', error));
+
+app.use(bodyParser.json());
 app.use('/graphql', expressGraphQL({
 	schema,
 	graphiql:true
