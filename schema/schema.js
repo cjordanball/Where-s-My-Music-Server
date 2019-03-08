fa174a8fd8b547aacce814fa4277c039fb78c1aa
Jordan Ball <jordanball@WTA-MBP525.fios-router.home>
2019-03-07 22:06:35 -0500

    add mongoose models

--- schema/schema.js
+++ schema/schema.js
@@ -1,45 +1,8 @@
 const graphql = require('graphql');
-const data = require('../sampleData.js');
+const { GraphQLSchema } = graphql;
 
-const {
-	GraphQLObjectType,
-	GraphQLString,
-	GraphQLSchema,
-	GraphQLList
-} = graphql
-
-const ScoreType = new GraphQLObjectType({
-	name: 'Score',
-	fields: () => ({
-		id: { type: GraphQLString },
-		name: { type: GraphQLString },
-		composer: { type: GraphQLString },
-		description: { type: GraphQLString }
-	})
-});
-
-const RootQuery = new GraphQLObjectType({
-	name: 'RootQueryType',
-	fields: {
-		scores: {
-			type: GraphQLList(ScoreType),
-			args:{},
-			resolve(parentValue, args) {
-				return data;
-			}
-		},
-		score: {
-			type: ScoreType,
-			args: { id: { type: GraphQLString } },
-			resolve(parentValue, args) {
-				return data.find(val => {
-					return val.id === args.id
-				})
-			}
-		}
-	}
-});
+const RootQueryType = require('./rootQuery');
 
 module.exports = new GraphQLSchema({
-	query: RootQuery
+	query: RootQueryType
 });
