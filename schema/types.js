7b8d22ade95026fda14ba1fd7a9ab73dd69c9c5c
C. Jordan Ball III <cjordanball@ballwebdev.com>
2019-03-08 20:04:18 -0500

    add sample queries

--- schema/types.js
+++ schema/types.js
@@ -1,22 +1,22 @@
 const mongoose = require('mongoose');
 const graphql = require('graphql');
 const {
-  GraphQLObjectType,
-  GraphQLList,
-  GraphQLID,
-  GraphQLInt,
-  GraphQLString
+	GraphQLObjectType,
+	GraphQLList,
+	GraphQLID,
+	GraphQLInt,
+	GraphQLString
 } = graphql;
 const Score = mongoose.model('score');
 
 const ScoreType = new GraphQLObjectType({
-  name:  'Score',
-  fields: () => ({
-    id: { type: GraphQLID },
-    name: { type: GraphQLString },
-    description: { type: GraphQLString },
-    composer: { type: GraphQLString }
-  })
+	name:  'Score',
+	fields: () => ({
+		id: { type: GraphQLID },
+		name: { type: GraphQLString },
+		description: { type: GraphQLString },
+		composer: { type: GraphQLString }
+	})
 });
 
 module.exports = ScoreType;
