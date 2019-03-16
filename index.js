cb024b7ba3f731d67b833864a0d8104dc8e21fb4
Jordan Ball <jordanball@WTA-MBP525.fios-router.home>
2019-03-15 22:41:48 -0400

    update schema for query

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const models = require('./models');
 const expressGraphQL = require('express-graphql');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const schema = require('./schema/schema');
 const app = express();
 
-const MONGO_URI = 'mongodb://cjb:1password@ds161345.mlab.com:61345/scores'
-mongoose.Promise = global.Promise
+const MONGO_URI = 'mongodb://cjb:1password@ds161345.mlab.com:61345/scores';
+if (!MONGO_URI) {
+	throw new Error('You must provide a MongoLab URI');
+}
+mongoose.Promise = global.Promise;
 mongoose.connect(MONGO_URI);
 mongoose.connection
 	.once('open', () => console.log('We are now connected!'))
