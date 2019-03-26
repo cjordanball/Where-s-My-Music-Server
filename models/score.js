9bcaea6747ecfcb922eb719391f8d27f2af684cd
Jordan Ball <jordanball@WTA-MBP525.local>
2019-03-26 11:28:55 -0400

    misc

--- models/score.js
+++ models/score.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ScoreSchema = new Schema({
+	_id: { type: String},
 	name: { type: String },
 	composer: { type: String },
-	descriptions: { type: String }
+	description: { type: String },
+	linkURL: { type: String }
 });
 
 mongoose.model('score', ScoreSchema);
